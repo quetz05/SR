18c64591f08190f1135dc2d1fabb33877193bf59
Quetz <quetz05@gmail.com>
2015-01-17 17:33:42 +0100

    Pierwsze commit -> dolaczenie ZeroMQ, ale polaczenie jeszcze nie dziala

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Server server = new Server();
+
+
+            Console.ReadKey();
+        }
+    }
+}
