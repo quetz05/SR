ded49bc4bd934e0e1c6314ee27ffa3833f3f7bfa
bdomagala <bdomagala@pkobp.pl>
2015-02-03 16:15:43 +0100

    Dalsza praca

--- Server/Responder.cs
+++ Server/Responder.cs
@@ -14,29 +14,31 @@ namespace SR
         private Queue<Message> queue;
         private Mutex mutex;
         private String type;
-        List<Member> Members;
+        List<Member> Clients;
+        List<Member> Servers;
+        List<Task> taskList;
 
-        public Responder(String type, List<Member> members)
+
+        public Responder(String type, List<Member> clients, List<Member> servers)
         {
             this.queue = new Queue<Message>();
             this.mutex = new Mutex();
             this.type = type;
-            this.Members = members;
-        
+            this.Clients = clients;
+            this.Servers = servers;
         }
 
         public void Run()
         {
             while(true)
             {
-                CheckHeartbeats();
+                CheckHeartbeats(Servers);
+                CheckHeartbeats(Clients);
                 CheckTasks();
                 Respond();
 
-
                 Thread.Sleep(1);
             }
-
 
 
         }
@@ -57,9 +59,9 @@ namespace SR
         }
 
 
-        private void CheckHeartbeats()
+        private void CheckHeartbeats(List<Member> members)
         {
-           foreach(var x in Members)
+            foreach (var x in members)
                if(x.alive)
                    if(x.session.HBTimer.ElapsedMilliseconds > Session.HB_TIME + Session.WAIT_TIME)
                    {
@@ -77,6 +79,13 @@ namespace SR
 
         }
 
+        private List<Member> ChooseMembers()
+        {
+
+
+            return Servers;
+        }
+
         private void Respond()
         {
             while(queue.Count != 0)
@@ -85,56 +94,58 @@ namespace SR
 
                 if (msg != null)
                 {
+                    List<Member> members = ChooseMembers();
+
                     switch (msg.type)
                     {
                         case Message.MessageType.HB:
                             {
-                                HB(msg);
+                                HB(msg, members);
                             }
                             break;
                         case Message.MessageType.CHECK_BLOCK:
                             {
-                                CHECK_BLOCK(msg);
+                                CHECK_BLOCK(msg, members);
                             }
                             break;
                         case Message.MessageType.SEM_CHECK:
                             {
-                                SEM_CHECK(msg);
+                                SEM_CHECK(msg, members);
                             }
                             break;
                         case Message.MessageType.SEM_CREATE:
                             {
 
-                                SEM_CREATE(msg);
+                                SEM_CREATE(msg, members);
                             }
                             break;
                         case Message.MessageType.SEM_DESTROY:
                             {
-                                SEM_DESTROY(msg);
+                                SEM_DESTROY(msg, members);
                             }
                             break;
                         case Message.MessageType.SEM_P:
                             {
-                                SEM_P(msg);
+                                SEM_P(msg, members);
                             }
                             break;
                         case Message.MessageType.SEM_V:
                             {
-                                SEM_V(msg);
+                                SEM_V(msg, members);
                             }
                             break;
-                        default: Console.WriteLine(type + "::" + DateTime.Now + "> Invalid MsgType from " + Members[msg.info.ipIndex].name);
+                        default: Console.WriteLine(type + "::" + DateTime.Now + "> Invalid MsgType from " + members[msg.info.ipIndex].name);
                             break;
                     }
                 }
                 else
-                    Console.WriteLine(type + "::" + DateTime.Now + "> Empty message from " + Members[msg.info.ipIndex].name);
+                    Console.WriteLine(type + "::" + DateTime.Now + "> Empty message from " + members[msg.info.ipIndex].name);
             }
 
 
         }
 
-        private void HB(Message msg)
+        private void HB(Message msg, List<Member> Members)
         {
             if (Members[msg.info.ipIndex].alive == false)
             {
@@ -149,37 +160,56 @@ namespace SR
             }
         }
 
-        private void SEM_CREATE(Message msg)
+        private void SEM_CREATE(Message msg, List<Member> Members)
         {
             Console.WriteLine(type + "::" + DateTime.Now + "> Receive CHECK_BLOCK from " + Members[msg.info.ipIndex].name);
 
+            if(Server.semaphores.Exist(msg.semOption.name))
+            {
+                //semafor istnieje na serwerze
+
+
+            }
+            else if (Server.fSemaphores.Exist(msg.semOption.name))
+            {
+                //semafor istnieje na innym znanym serwerze
+
+
+            }
+            else
+            {
+                // nowy task + zapytanie do innych serwerów
+
+
+            }
+
         }
 
-        private void SEM_DESTROY(Message msg)
+        private void SEM_DESTROY(Message msg, List<Member> Members)
         {
             Console.WriteLine(type + "::" + DateTime.Now + "> Receive SEM_CHECK from " + Members[msg.info.ipIndex].name);
 
         }
 
-        private void SEM_P(Message msg)
+        private void SEM_P(Message msg, List<Member> Members)
         {
             Console.WriteLine(type + "::" + DateTime.Now + "> Receive SEM_CREATE from " + Members[msg.info.ipIndex].name);
 
         }
 
-        private void SEM_V(Message msg)
+        private void SEM_V(Message msg, List<Member> Members)
         {
             Console.WriteLine(type + "::" + DateTime.Now + "> Receive SEM_DESTROY from " + Members[msg.info.ipIndex].name);
 
         }
 
-        private void SEM_CHECK(Message msg)
+        private void SEM_CHECK(Message msg, List<Member> Members)
         {
             Console.WriteLine(type + "::" + DateTime.Now + "> Receive SEM_P from " + Members[msg.info.ipIndex].name);
 
         }
 
-        private void CHECK_BLOCK(Message msg)
+        private void CHECK_BLOCK(Message msg, List<Member> Members)
         {
             Console.WriteLine(type + "::" + DateTime.Now + "> Receive SEM_V from " + Members[msg.info.ipIndex].name);
 
